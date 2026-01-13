27687150a8b69f1e6cee6028a65a8baa0f74bb73
William Edward Sugiharto <willsugiharto@gmail.com>
2026-01-13 15:28:52 +0800

    ok im DONE WITH UI

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@
 
 console.log("Welcome to rock paper scissors!");
 
+const container = document.querySelector("#container");
+let human_choice = ""; 
 
 /* Computer Choice */
 function getComputerChoice()
@@ -16,11 +18,10 @@ function getComputerChoice()
     }else{
         return 'scissors';
     }
-
 }
 
-/* Human Choice */
 
+/* Main game logic */
 function playRound(humanChoice, computerChoice)
 {
     humanChoice = humanChoice.toLowerCase();
@@ -32,41 +33,47 @@ function playRound(humanChoice, computerChoice)
     let human_win = false;
     if(humanChoice == 'paper')
     {
-        if(computerChoice == 'rock') {human_win = true; console.log(announcement_win);}
-        else if(computerChoice == 'scissors') {human_win = false; console.log(announcement_lose);}
+        if(computerChoice == 'rock') {human_win = true; intermittent_ui(announcement_win);}
+        else if(computerChoice == 'scissors') {human_win = false; intermittent_ui(announcement_lose);}
         else{
-            console.log(announcement_draw);
+            intermittent_ui(announcement_draw);
             return undefined;
         }
     }
 
     if(humanChoice == 'rock')
     {
-        if(computerChoice == 'scissors') {human_win = true; console.log(announcement_win);}
-        else if(computerChoice == 'paper') {human_win = false; console.log(announcement_lose);}
+        if(computerChoice == 'scissors') {human_win = true; intermittent_ui(announcement_win);}
+        else if(computerChoice == 'paper') {human_win = false; intermittent_ui(announcement_lose);}
         else{
-            console.log(announcement_draw)
+            intermittent_ui(announcement_draw);
             return undefined;
         }
     }
 
     if(humanChoice == 'scissors')
     {
-        if(computerChoice == 'paper') {human_win = true; console.log(announcement_win);}
-        else if(computerChoice == 'rock') {human_win = false; console.log(announcement_lose);}
+        if(computerChoice == 'paper') {human_win = true; intermittent_ui(announcement_win);}
+        else if(computerChoice == 'rock') {human_win = false; intermittent_ui(announcement_lose);}
         else{
-            console.log(announcement_draw)
+            intermittent_ui(announcement_draw);
             return undefined;
         }
     }
     return human_win
-
-    
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
+function intermittent_ui(txt)
+{
+    
+    document.querySelector('.outputer').remove();
+    console.log('help');
+    const outputer = document.createElement('div');
+    outputer.classList.add('outputer');
+    outputer.textContent = txt;
+    outputer.setAttribute('style', 'color:rgb(2, 9, 74); font-size: 28px')
+    container.insertAdjacentElement('afterend', outputer);
+}
 
 function checkWinner()
 {
@@ -80,22 +87,12 @@ function checkWinner()
         return 'play';
     }
 }
-const playerbox = document.querySelector("#playerbox");
-const compbox = document.querySelector("#compbox");
-
-const pscore = document.createElement("p");
-const cscore = document.createElement("p");
-pscore.textContent = playerScore;
-cscore.textContent = computerScore;
-playerbox.appendChild(pscore);
-compbox.appendChild(cscore);
-
 
 function updateUI()
 {
     
-      pscore.textContent = playerScore;
-      cscore.textContent = computerScore;
+      pscore.textContent = " "+playerScore;
+      cscore.textContent = " "+computerScore;
       playerbox.removeChild(pscore);
       compbox.removeChild(cscore);
 
@@ -111,61 +108,58 @@ function finalwinner(player)
     finalbox.textContent = `Game Over! ${player} Wins!!!`;
     finalbox.setAttribute('style', 'color:red; font-size:20px; padding:5px; margin:15px;');
     const ovr = document.querySelector('.ovr');
-    ovr.appendChild(finalbox);
+    ovr.insertAdjacentElement('afterend', finalbox);
+    document.querySelector('#scissors').setAttribute('disabled', 'disabled');
+    document.querySelector('#paper').setAttribute('disabled', 'disabled');
+    document.querySelector('#rock').setAttribute('disabled', 'disabled');
+    
 }
-    
-    
-    
-    
-    const container = document.querySelector("#container");
-    let human_choice = "";
 
-    container.addEventListener("click", (event) => {
-      event.preventDefault();
-      let target = event.target;
-      human_choice = target.id;
-      console.log("here human choice : " + human_choice);
+let playerScore = 0;
+let computerScore = 0;
 
-      let res = playRound(human_choice, getComputerChoice());
-      if (res == true) playerScore += 1;
-      else if(res == false) computerScore += 1;
+const outputer = document.createElement('div');
+outputer.classList.add('outputer');
+outputer.textContent = 'Game starts!';
+outputer.setAttribute('style', 'color: rgb(156, 47, 47); ');
+container.insertAdjacentElement('afterend', outputer);
 
-      updateUI();
-      let win = checkWinner();
-      if(win === 'human')
-      {
+
+const playerbox = document.querySelector("#playerbox");
+const compbox = document.querySelector("#compbox");
+
+const pscore = document.createElement("p");
+const cscore = document.createElement("p");
+pscore.textContent = " "+playerScore;
+cscore.textContent = " "+computerScore;
+playerbox.appendChild(pscore);
+compbox.appendChild(cscore);
+
+
+
+
+container.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let target = event.target;
+    human_choice = target.id;
+    console.log("here human choice : " + human_choice);
+
+    let res = playRound(human_choice, getComputerChoice());
+    if (res == true) playerScore += 1;
+    else if(res == false) computerScore += 1;
+
+    updateUI();
+    let win = checkWinner();
+    if(win === 'human')
+    {
         finalwinner('human');
-      }else if(win === 'computer')
-      {
+    }else if(win === 'computer')
+    {
         finalwinner('computer');
-      }
+    }
 
-      
-
-      human_choice = "";
-    });
+    human_choice = "";
+});
 
 
-
-/* Main */
-
-
-    /*
-    let decision = playRound(getHumanChoice(), getComputerChoice());
-    if(decision)playerScore+=1;
-    else if(decision == false) computerScore += 1;
-    
-
-    display_comp = 'Computer score: '+computerScore;
-    display_human = 'Human score: '+playerScore;
-    console.log('round: '+(i+1));
-    console.log(display_comp);
-    console.log(display_human);
-    console.log('\n');
-
-console.log('Final Verdict:')
-if(playerScore>computerScore) console.log('Player Wins!')
-else if(playerScore < computerScore) console.log('Computer Wins!');
-else console.log('Draw!');
-console.log('trial');
-*/
